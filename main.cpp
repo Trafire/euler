2cbe7538049b3c2e178645fdd996581ba4b7dccf
Trafire <antoinewood@gmail.com>
2019-02-28 18:17:43 -0500

    level two branch created

--- main.cpp
+++ main.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
 
-int euler1 (int target){
-    int sum = 0;
-    for (int i = 0; i < target; i++) {
-        if (i % 5 == 0 | i % 3 == 0) {
-            sum += i;
-        }
+/***
+ *
+ * @param Target Limit for Fib values
+ * @return Sum of even numbers below target in fib sequence
+ */
+
+int fib (int a, int b, int sum, int limit) {
+    int next_val = a + b;
+
+    // should stop when range exceeds target limit
+    if (next_val > limit) {
+        return sum;
     }
-    return sum;
+    // want sum of only even number
+    if (next_val % 2 == 0) {
+        sum +=  next_val;
+    }
+
+    return fib(b, next_val, sum, limit);
 }
 
-int main() {
-    std::cout << euler1(1000);
-    return 0;
+int main(int argc, char** argv) {
+    if (argc == 2) {
+        const int limit = atoi(argv[1]);
+        std::cout << fib(0,1,0, limit);
+        return 0;
+    }
+    else {
+        return 1;
+    }
+
 }
